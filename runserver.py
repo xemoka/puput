42206e0cf028676f5813269d4b216df86d385d5e
Marc <marctc@gmail.com>
2016-04-21 23:53:29 +0200

    [wip]travis

--- runserver.py
+++ runserver.py
@@ -12,7 +12,7 @@ def run_django():
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(['manage.py', 'migrate'])
-    execute_from_command_line(['manage.py', 'loaddata', 'tests/testapp/blog/fixtures/test_data.json'])
+    execute_from_command_line(['manage.py', 'loaddata', 'tests/testapp/fixtures/test_data.json'])
     execute_from_command_line(['manage.py', 'runserver'])
 
 
